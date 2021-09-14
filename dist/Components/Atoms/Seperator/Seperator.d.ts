40beaa08f8d9463095635b2b73654f41488571b8
Amir Sarkis <amir@symbiote.com.au>
2021-09-14 15:06:05 +1000

    Added updated build files

---
+++ dist/Components/Atoms/Seperator/Seperator.d.ts
@@ -0,0 +1,8 @@
+import React from "react";
+interface Props {
+    size: number;
+}
+export default class Seperator extends React.Component<Props> {
+    render(): JSX.Element;
+}
+export { Seperator };
