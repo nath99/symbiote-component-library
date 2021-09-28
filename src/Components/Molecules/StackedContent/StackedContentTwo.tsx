913ebff5ad6fd3b1196391af83d92e14d8128ef2
Amir Sarkis <amir@symbiote.com.au>
2021-09-28 15:41:57 +1000

    Added HTML rendering changes to StackedContent components

--- src/Components/Molecules/StackedContent/StackedContentTwo.tsx
+++ src/Components/Molecules/StackedContent/StackedContentTwo.tsx
@@ -20,12 +20,16 @@ class StackedContentTwo extends React.Component<Props> {
             <div className={`stacked-content ${className ? className : ""}`}>
                 <div className={`content-box${!content[0] ? " content-box--empty" : ""} box-1 col-12 col-md-10`} style={{ background: `${backgroundColors[0]}` }}>
                     <div className={`col-12 col-md-7 col-lg-5 ${foregroundColors[0] == "inverted" ? "inverted" : ""}`}>
-                        {content[0]}
+                        dangerouslySetInnerHTML={{
+                            __html:  content[0]
+                        }}
                     </div>
                 </div>
                 <div className={`content-box${!content[1] ? " content-box--empty" : ""} box-2 col-12 col-md-6 offset-md-6 col-lg-7 offset-lg-5`} style={{ background: `${backgroundColors[1]}` }}>
                     <div className={`col-12 col-lg-10 ${foregroundColors[1] == "inverted" ? "inverted" : ""}`}>
-                        {content[1]}
+                        dangerouslySetInnerHTML={{
+                            __html: content[1]
+                        }}
                     </div>
                 </div>
             </div>
