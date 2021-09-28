cdf4f7a2d6e7b870e88ba1993a5e82823558f98a
Amir Sarkis <amir@symbiote.com.au>
2021-09-28 15:46:16 +1000

    Reverted changes to HTML rendering for StackedContent component

--- src/Components/Molecules/StackedContent/StackedContentThree.tsx
+++ src/Components/Molecules/StackedContent/StackedContentThree.tsx
@@ -20,23 +20,17 @@ class StackedContentThree extends React.Component<Props> {
             <div className={`stacked-content ${className ? className : ""}`}>
                 <div className={`content-box${!content[0] ? " content-box--empty" : ""} box-1 col-12 col-md-10` } style={{ background: `${backgroundColors[0]}` }}>
                     <div className={`col-12 col-md-7 col-lg-5 ${foregroundColors[0] == "inverted" ? "inverted" : ""}`}>
-                        dangerouslySetInnerHTML={{
-                            __html: content[0]
-                        }}
+                        {content[0]}
                     </div>
                 </div>
                 <div className={`content-box${!content[1] ? " content-box--empty" : ""} box-2 col-12 col-md-6 offset-md-6 col-lg-7 offset-lg-5`} style={{ background: `${backgroundColors[1]}` }}>
                     <div className={`col-12 col-lg-10 ${foregroundColors[1] == "inverted" ? "inverted" : ""}`}>
-                        dangerouslySetInnerHTML={{
-                            __html: content[1]
-                        }}
+                        {content[1]}
                     </div>
                 </div>
                 <div className={`content-box${!content[2] ? " content-box--empty" : ""} box-3 col-12 col-md-6 offset-md-1 col-lg-7 offset-lg-2`} style={{ background: `${backgroundColors[2]}` }}>
                     <div className={`col-12 col-lg-10 ${foregroundColors[2] == "inverted" ? "inverted" : ""}`}>
-                        dangerouslySetInnerHTML={{
-                            __html: content[2]
-                        }}
+                        {content[2]}
                     </div>
                 </div>
             </div>
--- src/Components/Molecules/StackedContent/StackedContentTwo.tsx
+++ src/Components/Molecules/StackedContent/StackedContentTwo.tsx
@@ -4,7 +4,7 @@ interface Props {
     className?: string,
     backgroundColors: [string, string],
     foregroundColors: ["normal" | "inverted", "normal" | "inverted"],
-    content: React.ReactNode[]
+    content: [React.ReactNode, React.ReactNode]
 }
 
 class StackedContentTwo extends React.Component<Props> {
@@ -20,16 +20,12 @@ class StackedContentTwo extends React.Component<Props> {
             <div className={`stacked-content ${className ? className : ""}`}>
                 <div className={`content-box${!content[0] ? " content-box--empty" : ""} box-1 col-12 col-md-10`} style={{ background: `${backgroundColors[0]}` }}>
                     <div className={`col-12 col-md-7 col-lg-5 ${foregroundColors[0] == "inverted" ? "inverted" : ""}`}>
-                        dangerouslySetInnerHTML={{
-                            __html:  content[0]
-                        }}
+                        {content[0]}
                     </div>
                 </div>
                 <div className={`content-box${!content[1] ? " content-box--empty" : ""} box-2 col-12 col-md-6 offset-md-6 col-lg-7 offset-lg-5`} style={{ background: `${backgroundColors[1]}` }}>
                     <div className={`col-12 col-lg-10 ${foregroundColors[1] == "inverted" ? "inverted" : ""}`}>
-                        dangerouslySetInnerHTML={{
-                            __html: content[1]
-                        }}
+                        {content[1]}
                     </div>
                 </div>
             </div>
